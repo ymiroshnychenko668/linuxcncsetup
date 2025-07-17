eb6f85d6589d14491408e57256bfc265088a4b64
cnc <cnc@cnc.com>
2025-07-17 12:40:46 +0300

    fix linuxcnc checkout

--- upgrade-mesa.sh
+++ upgrade-mesa.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 # Install dependencies
@@ -8,6 +7,7 @@ sudo apt update
 sudo apt install -y build-essential git python3 python3-distutils python3-setuptools pkg-config libpci-dev libmd-dev automake autoconf libtool libmodbus-dev libusb-1.0-0-dev yapps2 intltool libboost-all-dev tk-dev libeditreadline-dev libxmu-dev asciidoc asciidoctor dblatex
 
 # Clone and build mesaflash
+cd "$HOME"
 if [ ! -d "$HOME/mesaflash" ]; then
     git clone https://github.com/LinuxCNC/mesaflash.git "$HOME/mesaflash"
 fi
@@ -17,29 +17,31 @@ make
 sudo make install
 
 # Clone latest HostMot2 firmware
+cd "$HOME"
 if [ ! -d "$HOME/hostmot2-firmware" ]; then
     git clone https://github.com/LinuxCNC/hostmot2-firmware.git "$HOME/hostmot2-firmware"
 else
     cd "$HOME/hostmot2-firmware" && git pull
 fi
 
-# Clone and build latest LinuxCNC drivers (hm2_eth)
-if [ ! -d "$HOME/linuxcnc" ]; then
-    git clone https://github.com/LinuxCNC/linuxcnc.git "$HOME/linuxcnc"
+# Clone and build latest LinuxCNC drivers (hm2_eth) into linuxcnc-sources
+cd "$HOME"
+if [ ! -d "$HOME/linuxcnc-sources" ]; then
+    git clone https://github.com/LinuxCNC/linuxcnc.git "$HOME/linuxcnc-sources"
 else
-    cd "$HOME/linuxcnc" && git pull
+    cd "$HOME/linuxcnc-sources" && git pull
 fi
 
-cd "$HOME/linuxcnc/src"
+cd "$HOME/linuxcnc-sources/src"
 ./autogen.sh
 ./configure --with-realtime=uspace
 make
 sudo make setuid
 
-#
-# Replace "your-username" with your actual non-root username
+# Use non-root username
 NON_ROOT_USER=$(logname)
 
+# Verify HAL pins as non-root user
 echo "Verifying HAL pins as $NON_ROOT_USER..."
 sudo -u "$NON_ROOT_USER" halrun <<EOF
 loadrt hostmot2
